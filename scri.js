e92361ed3e7237e2d99793fbf8043dda084aa28a
Ryan-lang10 <ryanhelena69@gmail.com>
2024-09-28 12:00:35 -0300

    Add files via upload

---
+++ scri.js
@@ -0,0 +1,18 @@
+function solicitarDados() {
+// solicita o nome do usario
+let time  = prompt("Qual e o nome do seu time?");
+// solicita o ano  de fundação do time
+let  ano = prompt("Qual e o ano de fundação do seu time?");
+// solicita o nome do treinador do time
+let treinador = prompt("Qual e o nome do seu treinador?");
+
+alert("voce torce pro " + time + " fundado em, " + ano + " e seu treinador" + " e o " + treinador +"");
+// mostrar os dados no console 
+console.log("nome do usario", time);
+console.log("ano de fundação do time",ano);
+console.log("nome do terinador ",treinador);
+
+
+
+
+}
